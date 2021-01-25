a98c4b5d9d6d7fdc2c5df51208f9e21016848772
Satya <satya@oak.works>
2021-01-25 12:54:05 -0500

    Updates

---
+++ precache-manifest.36ac41d63df3df72210ea002f4b31f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9114e1946406e305e6fa77fa1fab31f9",
+    "url": "/fortnite-characters/index.html"
+  },
+  {
+    "revision": "0379547f09d89f452b45",
+    "url": "/fortnite-characters/static/css/2.87583f7f.chunk.css"
+  },
+  {
+    "revision": "12ccb864f7ad0061bd21",
+    "url": "/fortnite-characters/static/css/main.0ced0ffa.chunk.css"
+  },
+  {
+    "revision": "0379547f09d89f452b45",
+    "url": "/fortnite-characters/static/js/2.6b9672d6.chunk.js"
+  },
+  {
+    "revision": "ec44ac7ef2dc1593eca7ce4755c8522c",
+    "url": "/fortnite-characters/static/js/2.6b9672d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ccb864f7ad0061bd21",
+    "url": "/fortnite-characters/static/js/main.7ece3ce7.chunk.js"
+  },
+  {
+    "revision": "8e72e57f21478b64bd01",
+    "url": "/fortnite-characters/static/js/runtime-main.768090ad.js"
+  }
+]);
